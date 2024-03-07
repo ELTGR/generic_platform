c3f6fcd42617f3e0442ff379581124fc9e0fb0bb
ELTGR <eliott.deleplanque.pro@gmail.com>
2024-03-07 11:17:49 +0100

    Change name fision of bridge and implementation

--- environments/My_Custom_Env/views2d.py
+++
@@ -1,9 +0,0 @@
-from environments.My_Custom_Env.bridge import UUV
-
-
-'''
-In this class, write the functions useful to draw your envy in 2d
-'''
-class MyCustomEnv2dView:
-    pass
-
--- utils_platform.py
+++ utils_platform.py
@@ -12,10 +12,10 @@ from time import sleep
 from gymnasium import spaces
 from ray import tune    
 from ray.air import CheckpointConfig
-import torch 
-import tensorflow as tf
 
-class UtilsPlatform():
+
+
+class DrlExperiments():
 
     def __init__(self,env_config,env) :
 
@@ -24,8 +24,7 @@ class UtilsPlatform():
         
     def train(self,train_config) : 
         self.env_config['implementation'] = "simple"
-        ray.init()
-
+        
         tune_config={
                                     "env": self.env_type,
                                     "env_config":self.env_config,
@@ -38,22 +37,14 @@ class UtilsPlatform():
                                     "optimizer": train_config["optimizer"],
                                 }
         
-        
+        ray.init()
         algo = tune.run("PPO",name=train_config["name"],config = tune_config,stop = {"timesteps_total": train_config["stop_step"]}, 
                         checkpoint_config = CheckpointConfig(checkpoint_at_end=True,checkpoint_frequency=train_config["checkpoint_freqency"] ),
                         storage_path=train_config["path"]
                         )
                 
-        # Retrieve the best trial
-        best_trial = algo.get_best_trial("episode_reward_mean", mode="max")
-
-        # Get the trained model from the best trial
-        trained_model = best_trial.get_model()
-
-        # Save the trained model (assuming it's a PyTorch model)
-        torch.save(trained_model.state_dict(), "path/to/save/trained_model.pth")
-
-                                                                    
+      
+                                                                  
     def train_from_checkpoint(self,train_config,path):
             
         self.env_config['implementation'] = "simple"
@@ -84,14 +75,14 @@ class UtilsPlatform():
             print("config : ",self.env_config)
 
             env = self.env_type(env_config = self.env_config)
-            loaded_model = tf.keras.models.load_model(path)
+            loaded_model = Algorithm.from_checkpoint(path)
             agent_obs = env.reset()
             print("obs",agent_obs)
             env.render()
 
             while True : 
 
-                action =  action = loaded_model.predict(agent_obs)
+                action =  action = loaded_model.compute_single_action(agent_obs)
                 print(action)
                 agent_obs, reward, done, info = env.step(action)
                 print("obs",agent_obs)
@@ -103,4 +94,49 @@ class UtilsPlatform():
                     agent_obs = env.reset()
                     print("obs",agent_obs)
                     env.render()
- 
+
+
+
+
+
+if __name__ == '__main__':
+
+
+    from Scenarios.UUV_Mono_Agent_TSP.env import UUVMonoAgentTSPEnv
+
+
+    taille_map_x = 3
+    taille_map_y = 3
+    n_orders = 3
+    step_limit = 100
+
+
+    env_config={
+                "implementation":"simple",
+                
+                "num_boxes_grid_width":taille_map_x,
+                "num_boxes_grid_height":taille_map_y,
+                "n_orders" : n_orders,
+                "step_limit": step_limit,
+                "same_seed" : False
+                }
+
+    train_config = {
+                    "name" : str(taille_map_x)+"x"+str(taille_map_y)+"_"+str(n_orders)+"_"+str(step_limit),
+                    "path" : "/home/ia/Desktop/generic_platform/Scenarios/UUV_Mono_Agent_TSP/models",
+                    "checkpoint_freqency" : 10,
+                    "stop_step" : 1000,
+                    "num_workers": 1,
+                    "num_learner_workers" : 0,
+                    "num_gpus": 0,
+                    "num_gpus_per_worker": 0,#
+                    "num_cpus_per_worker": 5,
+                    "model":{"fcnet_hiddens": [64, 64],},  # Architecture du réseau de neurones (couches cachées) 
+                    "optimizer": {"learning_rate": 0.001,} # Taux d'apprentissage
+    }
+
+    my_platform = DrlExperiments(env_config=env_config,env = UUVMonoAgentTSPEnv)
+    #my_platform.train(train_config=train_config) 
+
+    #my_platform.test(implementation="simple",path="/home/ia/Desktop/generic_platform/Scenarios/UUV_Mono_Agent_TSP/models/3x3_3_100/PPO_UUVMonoAgentTSPEnv_8751c_00000_0_2024-03-07_11-07-39/checkpoint_000000")
+    my_platform.train_from_checkpoint(train_config=train_config,path="/home/ia/Desktop/generic_platform/Scenarios/UUV_Mono_Agent_TSP/models/3x3_3_100/PPO_UUVMonoAgentTSPEnv_8751c_00000_0_2024-03-07_11-07-39/checkpoint_000000")
