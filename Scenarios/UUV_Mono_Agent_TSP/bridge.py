64bde8332171db61e078c95f27a7668c98ab94ad
ELTGR <eliott.deleplanque.pro@gmail.com>
2024-03-12 08:50:45 +0100

    only python

--- Scenarios/UUV_Mono_Agent_TSP/bridge.py
+++ Scenarios/UUV_Mono_Agent_TSP/bridge.py
@@ -3,8 +3,6 @@ import numpy as np
 import math
 import random
 
-from Scenarios.UUV_Mono_Agent_TSP.bluerov2_concrete_implementation.bluerov_node import BlueRov
-
 class MobileCommonInterface:
     
     def move_to(self, coordinates):
@@ -42,44 +40,44 @@ class UUVSimpleImplementation(MobileCommonInterface):
     def get_info(self) :  
         return "Je suis une implémentation simple"
     
-class Bluerov2RealImplementation(MobileCommonInterface):
+# class Bluerov2RealImplementation(MobileCommonInterface):
     
-    """
-        This class was created to build the bridge between functions/algorithms used to control 
-        Bluerov2 and the Deep Reinforcement Learning script that makes decisions. Use this class 
-        if you are using ArduSub and Unity.
-    """
+#     """
+#         This class was created to build the bridge between functions/algorithms used to control 
+#         Bluerov2 and the Deep Reinforcement Learning script that makes decisions. Use this class 
+#         if you are using ArduSub and Unity.
+#     """
 
-    def __init__(self,port_n):
+#     def __init__(self,port_n):
 
-        self.current_position = np.zeros(3)
+#         self.current_position = np.zeros(3)
 
-        self.ip_port = 14550 + port_n
-        n_bluerov = port_n
-        self.bluerov = BlueRov(device='udp:localhost:'+str(self.ip_port),nbr_bluerov=n_bluerov)
-        print(self.ip_port)
+#         self.ip_port = 14550 + port_n
+#         n_bluerov = port_n
+#         self.bluerov = BlueRov(device='udp:localhost:'+str(self.ip_port),nbr_bluerov=n_bluerov)
+#         print(self.ip_port)
 
-    def move_to(self, coordinates):
-        #print("Bluerov2RealImplementation move_to: ",coordinates)
+#     def move_to(self, coordinates):
+#         #print("Bluerov2RealImplementation move_to: ",coordinates)
         
-        goal = [coordinates[0] * 10, coordinates[1] * 10, -20 ]
-        self.bluerov.do_rali(goal)
+#         goal = [coordinates[0] * 10, coordinates[1] * 10, -20 ]
+#         self.bluerov.do_rali(goal)
 
-        curren_pos  = self.bluerov.get_position()
-        self.current_position[0], self.current_position[1] = curren_pos[0], curren_pos[1]
+#         curren_pos  = self.bluerov.get_position()
+#         self.current_position[0], self.current_position[1] = curren_pos[0], curren_pos[1]
        
 
 
-    def get_pos(self):
-        return self.bluerov.get_position()
+#     def get_pos(self):
+#         return self.bluerov.get_position()
     
-    def set_pos(self,new_pose):
-        #print("Bluerov2RealImplementation set_pos: ",new_pose[0],new_pose[1])
-        self.move_to(new_pose)
+#     def set_pos(self,new_pose):
+#         #print("Bluerov2RealImplementation set_pos: ",new_pose[0],new_pose[1])
+#         self.move_to(new_pose)
 
         
-    def get_info(self) :  
-        return "Je suis une implémentation real"
+#     def get_info(self) :  
+#         return "Je suis une implémentation real"
 
 
 class UUV:
@@ -92,7 +90,7 @@ class UUV:
             self.implementation = UUVSimpleImplementation()
         elif implementation == "real":
             print("in uxv real")
-            self.implementation = Bluerov2RealImplementation(ip_port)
+            self.implementation = UUVSimpleImplementation()
         else : 
             raise ValueError("Incorrect implementation value. Choose 'bluerov2' or 'simple'.")
 
